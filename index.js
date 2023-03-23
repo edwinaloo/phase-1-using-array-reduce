f35bcbc7bc3ccd3bdb3229ff2c038a31ea6f3719
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-23 09:43:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function(total, current) {
+  return total + current;
+}, 0);
+
+console.log(totalBatteries);
+
+
 // Code your solution here
